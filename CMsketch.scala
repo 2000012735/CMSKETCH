2d55922f7d5a91475d56f987f96e0476b1357732
2000012735 <127952616+2000012735@users.noreply.github.com>
2023-03-16 17:00:39 +0800

    Add files via upload

--- CMsketch.scala
+++ CMsketch.scala
@@ -1,7 +1,7 @@
 
 import chisel3._
 import chisel3.util._
-
+import scala.math
 class CMsketch(w:Int , d:Int) extends Module {
   val io = IO(new Bundle {
     val datain = Input(UInt(10.W))
@@ -13,59 +13,112 @@ class CMsketch(w:Int , d:Int) extends Module {
 
 
   def Hash(x: UInt, n: UInt, m: Int): UInt = {
-    val k = RegInit(0.U(32.W))
-    k := (m.U<<x)
+    val k = Wire(UInt(32.W))
+    k := (x<<m.U)
     k%n
   }
 
   def countgen(max: UInt, en: Bool): UInt = {
-    val cnt = RegInit(0.U(max.getWidth.W))
+    val cnt = RegInit(0.U(32.W))
     when(en) {
       cnt := Mux(cnt === max-1.U, 0.U, cnt + 1.U)
     }
     cnt
   }
-  val Hashin = Reg(Vec(d, UInt(log2Up(w).W)))
-  val Hashrd = Reg(Vec(d, UInt(log2Up(w).W)))
-  val counter = Reg(Vec(w * d, UInt(32.W)))
-  val enable = Reg(Vec(w * d, Bool()))
+  val Hashin = Wire(Vec(d, UInt(log2Up(w).W)))
+  val Hashrd = Wire(Vec(d, UInt(log2Up(w).W)))
+  val counter =Wire(Vec(w * d, UInt(32.W)))
+  val enable = Wire(Vec(w * d, Bool()))
 
-  when(io.wren) {
-    for (i <- 0 until d) {
-      Hashin(i) := Hash(io.datain,w.U,i)
+  when(io.wren){
+  for (i <- 0 until d) {
+    for (j <- 0 until w) {
+      enable(i * w + j) := j.U === Hashin(i)
     }
+  }}.otherwise{
     for (i <- 0 until d) {
       for (j <- 0 until w) {
-        enable(i*w+j) := j.U === Hashin(i)
+        enable(i * w + j) := false.B
       }
     }
+  }
+
+  for (i <- 0 until d) {
+    when(io.wren) {
+      Hashin(i) := Hash(io.datain, w.U, i + 1)
+    }.otherwise{
+      Hashin(i) := w.U
+    }
+  }
+
+
     for (i <- 0 until d) {
       for (j <- 0 until w) {
         counter(i*w+j) := countgen(10000000.U, enable(i*w+j))
       }
     }
-  }
-  val dataoutpre = Reg(Vec(d+1, UInt(32.W)))
+
+
+  val dataoutpre = Wire(Vec(d+1,UInt(32.W)))
       dataoutpre(d) := 10000000.U(32.W)
+
+  when(io.rden){
+    for(i <- 0 until d){
+      dataoutpre(i) := counter(i.U*w.U + Hashrd(i))
+    }
+  }.otherwise{
+    for (i <- 0 until d) {
+      dataoutpre(i) := 0.U
+    }
+  }
+
   val datamin = Reg(UInt(32.W))
 
   when(io.rden) {
     for (i <- 0 until d) {
-      Hashrd(i) := Hash(io.datard,w.U,i)
+      Hashrd(i) := Hash(io.datard,w.U,i+1)
     }
-
-    for (i <- 0 until (d)) {
-      dataoutpre(i) := counter(Hashrd(i) + w.U * i.U )
-    }
-    for (i <- 0 until (d)) {
-      when(dataoutpre(i) >= dataoutpre(i+1)) {
-        datamin := dataoutpre(i+1)
-      }
+  }.otherwise{
+    for (i <- 0 until d) {
+      Hashrd(i) := 0.U
     }
   }
+//  val indexmin = Wire(UInt(32.W))
+//  val small    = Wire(Vec(d*d,Bool()))
+//  for (i <- 0 until d){
+//    for(j <-0 until d){
+//      small(i*d+j) := dataoutpre(i)<=dataoutpre(j)
+//    }
+//  }
+//  val smallx   =Wire(Vec(d,Bool()))
+//  for (i <- 0 until(d)){
+//    for(j <- 0 until(d)){
+//      smallx(i) := smallx(i)&small(i*d+j)
+//    }
+//  }
+//  for (i<- 0 until(d)){
+//    when(smallx(i)){
+//      indexmin := i.U
+//    }
+//  }
+  when(io.rden){
+    for(i<-0 until(d)){
+      when (i.U===0.U){
+        datamin := dataoutpre(0)
+      }.otherwise{
+        when(dataoutpre(i)<datamin){
+          datamin := dataoutpre(i)
+        }
+      }
+    }
+  }.otherwise{
+    datamin := 0.U
+  }
+
   io.dataout := Mux(io.rden,datamin,0.U)
 }
 
+
 object CMsketch extends App{
-  Driver.emitVerilog(new CMsketch(90,5))
+  Driver.emitVerilog(new CMsketch(100,5))
 }
